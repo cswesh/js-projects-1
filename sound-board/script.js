107a27c316d10d7f115a8f7f856b928aacdb8061
cswesh <cswesh@yahoo.com>
2021-01-22 20:39:07 +0800

    rest of the simple js project added to the set

---
+++ sound-board/script.js
@@ -0,0 +1,27 @@
+const sounds =[
+    'notif1',
+    'notif2',
+    'funny',
+    'laugh',
+    'cartoon'
+];
+
+sounds.forEach((sound)=>{
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+    btn.addEventListener('click',()=>{
+        stopSongs();
+        document.getElementById(sound).play();
+    })
+    document.body.appendChild(btn);
+});
+
+function stopSongs(){
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    })
+}
